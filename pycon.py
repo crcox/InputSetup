6d465b201ac098a30b465eb570c9a031e92da2b4
Chris Cox <crcox@wisc.edu>
2015-01-28 16:53:19 -0600

    made setupJobs more general. Moved my hack-ish argparse work around into pycon.

--- pycon.py
+++ pycon.py
@@ -2,6 +2,12 @@ import tarfile
 import shutil
 import os
 
+class Args:
+    def __init__(self):
+        self.master = ''
+        self.run = False
+args = Args()
+
 def build(pkg,chtcrun='./ChtcRun'):
     """Given a package and a path to a current ChtcRun/ directory tree, execute
     the steps necessary to compile the package for use on condor."""
@@ -63,3 +69,69 @@ location of the interpreter on remote jobs.
             shutil.copyfileobj(of, nf)
 
     os.chmod(new,0755)
+
+def argparse(arglst,pargnames,argtype,flags):
+    """ This is a lame substitute for argparse that should bail us out on the
+    CHTC submit node where Python 2.6 is used. Probably buggy, definitely
+    limited. Excepts a dictionary of arguments paired with keys. The keys will
+    be the parameter names, and the arguments themselves are the values.
+
+    How to specify ``flags'':
+    flags will be a dictionary where keys are possible flags and values are the
+    variable names where the argument values will be stored within the program.
+    It is possible for multiple keys to point to the same variable (e.g., to
+    support short and long version of the flag).
+
+    How to specify ``argtype'':
+    In general, argtype will be a dictionary where keys are variable names
+    (e.g., the values in the flags dictionary) and the value will dictate what
+    sort of input is expected for that variable. Possible values are:
+   -  integer: declares the expected number of inputs associated with that
+      flag.
+   -  (empty) list: the number of inputs is taken to be > 0 but otherwise
+      unbounded. Everything following this flag, up until the next flag, will
+      be associated with this variable.
+   -  logical (True or False): Indicates that no values are expected to follow
+      the flag. The presence of the flag will toggle the value provided in
+      ``argtype''. So, if flags={'-a': 'a'} and argtype = {'a': True}, then the
+      presence of the -a flag will toggle the True to False.  """
+    class ARGS:
+        def __init__(self):
+            self._type="args"
+    args = ARGS()
+
+    # Positional arguments come first. Pair with values and remove them from
+    # arglst.
+    for name in pargnames:
+        setattr(args, name, arglst.pop(0))
+
+    # Parse kwargs
+    while len(arglst) > 0:
+        flag = arglst.pop(0)
+        try:
+            arg = flags[flag]
+        except KeyError:
+            print '\nERROR: {f} is not a known flag\n'.format(f=flag)
+            raise KeyError
+
+        if isinstance(argtype[arg],list):
+            x = []
+            while arglst[0] not in argtype.keys():
+                val = arglst.pop(0)
+                x.append(val)
+        elif isinstance(argtype[arg],int):
+            n = argtype[arg]
+            x = []
+            if n == 1:
+                val = arglst.pop(0)
+                x = val
+            elif n > 1:
+                for i in range(n):
+                    x.append(arglst.pop(0))
+        elif isinstance(argtype[arg],bool):
+            x = not argtype[arg]
+
+        setattr(args, arg, x)
+
+    return args
+
--- setupJobs.py
+++ setupJobs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import nextmds as mds
 import csv
 import json
 import os
@@ -8,6 +7,7 @@ import sys
 from datetime import datetime
 import tarfile
 import shutil
+import pycon
 
 try:
     import argparse
@@ -17,18 +17,6 @@ try:
         help="In addition to setting up the directory structure, run all analyses. Useful when the analysis is being done locally.")
     args = p.parse_args()
 except ImportError:
-    class Args:
-        def __init__(self):
-            self.master = ''
-            self.run = False
-    args = Args()
-
-    # Log required positional args
-    args.master = sys.argv[1]
-
-    # Specify translation from valid flags to valid arguments.
-    flagDict  = {'-r':'run','--r':'run'}
-
     # Specify type of input associated with argument:
     #   If you specify an integer, that declares the expected
     #   number of inputs.
@@ -37,36 +25,15 @@ except ImportError:
     #   If you specify a logical value, this means that the flag
     #   takes no inputs, and it's presence will flip this default
     #   value.
-    tmp = {'run',False}
+    pargnames = ['master']
+    argtype = {'run':False}
 
-    # Parse inputs and arguments
-    lst = sys.argv[2:]
-    while len(lst) > 0:
-        flag = lst.pop(0)
-        try:
-            arg = flagDict[flag]
-        except KeyError:
-            print '\nERROR: {f} is not a known flag\n'.format(f=flag)
-            raise KeyError
+    # Specify translation from valid flags to valid arguments.
+    flags = {'-r':'run','--r':'run'}
 
-        if isinstance(tmp[arg],list):
-            while lst[0] not in tmp.keys():
-                val = lst.pop(0)
-                tmp[arg].append(val)
-        elif isinstance(tmp[arg],int):
-            n = tmp[arg]
-            if n == 1:
-                val = lst.pop(0)
-                tmp[arg] = val
-            elif n > 1:
-                tmp[arg]=[0]*n
-                for i in range(n):
-                    tmp[arg][i] = lst.pop(0)
-        elif isinstance(tmp[arg],bool):
-            tmp[arg] = not temp[arg]
-
-    # Assign inputs into an args object as would be produced by argparse.
-    args.run = tmp['run']
+    # Parse the arguments
+    arglst = sys.argv[1:]
+    args = pycon.argparse(arglst, pargnames, argtype, flags)
 
 #############################################################
 #   Load data and parameters from the "master" json file    #
@@ -85,7 +52,7 @@ except KeyError:
     # This just ensures there is a list to loop over.
     # The idea is to initialize currentConfig with defaults and then update
     # those with the paramaters from each config dict.
-    allConfigs = [jdat['config']]
+    allConfigs = [jdat]
 
 #############################################################
 #  Define a root folder (current directory if not a condor  #
@@ -99,11 +66,8 @@ except KeyError:
     pass
 
 #############################################################
-#   Parse NEXT responses and write data to shared folder    #
+#                 Setup directory structure                 #
 #############################################################
-responsepath = jdat['responses']
-responses = mds.read_triplets(responsepath)
-
 sharedir = os.path.join(rootdir,'shared')
 if not os.path.isdir(sharedir):
     os.makedirs(sharedir)
@@ -112,25 +76,60 @@ archivedir = os.path.join(rootdir,'archive')
 if not os.path.isdir(archivedir):
     os.makedirs(archivedir)
 
-with open(os.path.join(sharedir,'queries_random.csv'), 'wb') as f:
-    writer = csv.writer(f)
-    writer.writerows(responses['RANDOM'])
+#############################################################
+#      Copy shared source and binary files into place       #
+#############################################################
+try:
+    shutil.copy(jdat['binary'],sharedir)
+except KeyError:
+    print "No shared binary indicated."
 
-with open(os.path.join(sharedir,'queries_adaptive.csv'), 'wb') as f:
-    writer = csv.writer(f)
-    writer.writerows(responses['ADAPTIVE'])
+try:
+    src_files = os.listdir(jdat['srcdir'])
+    for file_name in src_files:
+        full_file_name = os.path.join(jdat['srcdir'], file_name)
+        if (os.path.isfile(full_file_name)):
+            shutil.copy(full_file_name, sharedir)
+except KeyError:
+    print "No shared source directory indicated."
 
-with open(os.path.join(sharedir,'queries_cv.csv'), 'wb') as f:
-    writer = csv.writer(f)
-    writer.writerows(responses['CV'])
+#############################################################
+#       Copy shared data / Write shared URLS file           #
+#############################################################
+URLS = os.path.join(sharedir,'URLS')
+try:
+    if isinstance(jdat['data'],list):
+        datalst = jdat['data']
+    else:
+        datalst = [jdat['data']]
 
-with open(os.path.join(sharedir,'labels.txt'), 'w') as f:
-    for label in responses['labels']:
-        f.write(label+'\n')
+    with open(URLS,'w') as f:
+        for x in datalist:
+            pathparts = os.path.split(x)
+            if pathparts[0] == '/squid':
+                p = os.path.join('/',*pathparts[1:])
+                f.write(p+'\n')
+            else:
+                shutil.copy(x,sharedir)
+except KeyError:
+    print "No shared data files indicated."
 
-querydata = {k: responses[k] for k in ['nqueries','nitems']}
-with open(os.path.join(sharedir,'querydata.json'), 'wb') as f:
-    json.dump(querydata,f)
+try:
+    if isinstance(jdat['metadata'],list):
+        datalst = jdat['metadata']
+    else:
+        datalst = [jdat['metadata']]
+
+    with open(URLS,'a') as f:
+        for x in datalist:
+            pathparts = os.path.split(x)
+            if pathparts[0] == '/squid':
+                p = os.path.join('/',*pathparts[1:])
+                f.write(p+'\n')
+            else:
+                shutil.copy(x,sharedir)
+except KeyError:
+    print "No shared metadata indicated."
 
 #############################################################
 #        Loop over configs (if condor, do setup only)       #
@@ -148,22 +147,64 @@ for i, cfg in enumerate(allConfigs):
     currentConfig.update(cfg)
 
     jobdir = os.path.join(rootdir,'{cfgnum:03d}'.format(cfgnum=i))
-    modelfile = os.path.join(jobdir,'model.csv')
-    if os.path.isdir(jobdir):
-        if os.path.isfile(modelfile):
-            print "WARNING: {d} already contains model.csv; skipping.".format(d=jobdir)
-            continue
-    else:
-        os.makedirs(jobdir)
+    os.makedirs(jobdir)
 
-    cfgfile = os.path.join(jobdir,'config.json')
+    cfgfile = os.path.join(jobdir,'params.json')
+
+    #############################################################
+    #       Copy job source and binary files into place         #
+    #############################################################
+    try:
+        shutil.copy(cfg['binary'],sharedir)
+    except KeyError:
+        pass
+
+    try:
+        src_files = os.listdir(cfg['srcdir'])
+        for file_name in src_files:
+            full_file_name = os.path.join(cfg['srcdir'], file_name)
+            if (os.path.isfile(full_file_name)):
+                shutil.copy(full_file_name, sharedir)
+    except KeyError:
+        pass
+
+    #############################################################
+    #          Copy job data / Write shared URLS file           #
+    #############################################################
+    URLS = os.path.join(sharedir,'URLS')
+    try:
+        if isinstance(cfg['data'],list):
+            datalst = cfg['data']
+        else:
+            datalst = [cfg['data']]
+
+        with open(URLS,'w') as f:
+            for x in datalist:
+                pathparts = os.path.split(x)
+                if pathparts[0] == '/squid':
+                    p = os.path.join('/',*pathparts[1:])
+                    f.write(p+'\n')
+                else:
+                    shutil.copy(x,sharedir)
+    except KeyError:
+        pass
+
+    try:
+        if isinstance(cfg['metadata'],list):
+            datalst = cfg['metadata']
+        else:
+            datalst = [cfg['metadata']]
+
+        with open(URLS,'a') as f:
+            for x in datalist:
+                pathparts = os.path.split(x)
+                if pathparts[0] == '/squid':
+                    p = os.path.join('/',*pathparts[1:])
+                    f.write(p+'\n')
+                else:
+                    shutil.copy(x,sharedir)
+    except KeyError:
+        pass
+
     with open(cfgfile,'wb') as f:
         json.dump(currentConfig, f, sort_keys=True, indent=2, separators=(',', ': '))
-
-if args.run:
-    for i in range(len(allConfigs)):
-        jobdir = os.path.join(rootdir,'{cfgnum:03d}'.format(cfgnum=i))
-        mds.runJob(jobdir)
-
-    if jdat['archive']:
-        archive(archivedir)
